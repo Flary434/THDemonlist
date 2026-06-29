3ebffa84dcbc2c9f27355dc475a0daf26a13681b
nolli <towkeyneverclear@gmail.com>
2026-06-29 11:57:44 +0700

    removing "does not accept new records" lable

--- js/pages/List.js
+++ js/pages/List.js
@@ -47,8 +47,7 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>
-                        <p v-if="selected + 1 <= 150"><strong>100%</strong> or better to qualify</p>
-                        <p v-else>This level does not accept new records.</p>
+                        <p v-if="selected + 1"><strong>100%</strong> or better to qualify</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
                             <td class="user">
